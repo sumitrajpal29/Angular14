6c213f6dda9d5256ad1cca619475d961bce8e5f1
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2023-02-09 21:08:49 +0530

    *ngFor, ng-container

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Book {
+  name: string,
+  author: string,
+  img: string
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
+
+
 export class BooksComponent implements OnInit {
   isHidden: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  buttonText: string = "Hide Books";
-  changeButtonText() {
-    this.buttonText = "Hide Books" ? "Show Books" : "Hide Books";
-  }
+
+  books: Book[] = [
+    {
+      name: "Clean Code",
+      author: "Robert C. Martin",
+      img: "https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg"
+    },
+    {
+      name: "Cracking the Coding Interview",
+      author: "Gayle Laakmann McDowell",
+      img: "https://m.media-amazon.com/images/I/410hiaPGyCL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+    },
+    {
+      name: "Thinking in Java",
+      author: "Bruce Eckel",
+      img: "https://m.media-amazon.com/images/I/61-ZAFCWcLL._SX376_BO1,204,203,200_.jpg"
+    }
+  ]
+
+
+
+
 
 }
