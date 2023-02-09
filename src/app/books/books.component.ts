0a8fad57bf70924ff8b4fee8c7edba5ddf796571
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2023-02-10 02:19:54 +0530

    pipes, style component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -5,6 +5,7 @@ interface Book {
   name: string,
   author: string,
   img: string
+  price:number
 }
 
 @Component({
@@ -16,7 +17,6 @@ interface Book {
 
 export class BooksComponent implements OnInit {
   isHidden: boolean = true;
-
   constructor() { }
 
   ngOnInit(): void {
@@ -24,19 +24,22 @@ export class BooksComponent implements OnInit {
 
   books: Book[] = [
     {
-      name: "Clean Code",
-      author: "Robert C. Martin",
-      img: "https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg"
+      name: "clean code",
+      author: "robert c. martin",
+      img: "https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg",
+      price:950
     },
     {
       name: "Cracking the Coding Interview",
       author: "Gayle Laakmann McDowell",
-      img: "https://m.media-amazon.com/images/I/410hiaPGyCL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+      img: "https://m.media-amazon.com/images/I/410hiaPGyCL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      price:760
     },
     {
       name: "Thinking in Java",
       author: "Bruce Eckel",
-      img: "https://m.media-amazon.com/images/I/61-ZAFCWcLL._SX376_BO1,204,203,200_.jpg"
+      img: "https://m.media-amazon.com/images/I/61-ZAFCWcLL._SX376_BO1,204,203,200_.jpg",
+      price:4930
     }
   ]
 
